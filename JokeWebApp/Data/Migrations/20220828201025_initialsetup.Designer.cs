cce1c55367f8f8a1bfce6b71e904ca587fb6f8c3
camsh69 <camsh69@gmail.com>
2022-08-28 21:17:23 +0100

    Created Database

--- JokeWebApp/Data/Migrations/20220828201025_initialsetup.Designer.cs
+++ JokeWebApp/Data/Migrations/20220828201025_initialsetup.Designer.cs
@@ -4,6 +4,7 @@ using JokeWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JokeWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220828201025_initialsetup")]
+    partial class initialsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
